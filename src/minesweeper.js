34a56da8eae1bc23f266ed6fc4b326408f86457d
Balaruth <g.donoghue@outlook.com>
2018-01-28 09:54:38 +0100

    Board is now completely dynamically generated instead of hard coded

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,19 +1,29 @@
-const printBoard = (board) => {
-  console.log('Current Board:');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows.length; rowIndex++) {
+    let row = [];
+      for (let columnIndex = 0; columnIndex < numberOfColumns.length; columnIndex++) {
+        row.push(' ');
+      } board.push(row);
+  } return board;
 };
 
-let board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
+generatePlayerBoard(3, 3);
 
-printBoard(board);
-
-board[0][1] = '1';
-board[2][2] = 'B';
-
-printBoard(board);
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows.length; rowIndex++) {
+    let row = [];
+      for (let columnIndex = 0; columnIndex < numberOfColumns.length; columnIndex++) {
+        row.push(null);
+      } board.push(row);
+  } return board;
+      numberOfBombsPlaced = 0
+      // while loop can potentially place bombs on existing bombs. To be fixed
+        while (numberOfBombsPlaced < numberOfBombs) {
+          let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+          let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+          board[randomRowIndex][randomColumnIndex] = 'B';
+          numberOfBombsPlaced+=1;
+        }
+};
